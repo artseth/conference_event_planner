e03ccda4704f655f97cad073bdc782fde146998d
artseth <artsethmedia01@gmail.com>
2026-01-29 23:11:08 +0200

    ConferenceEvent.jsx

--- src/ConferenceEvent.jsx
+++ src/ConferenceEvent.jsx
@@ -69,7 +69,28 @@ const ConferenceEvent = () => {
 
   return (
     <>
-     
+      <navbar className="navbar_event_conference">
+        <div className="company_logo">Conference Expense Planner</div>
+        <div className="left_navbar">
+               <div className="nav_links">
+                 <a href="#venue" onClick={() => navigateToProducts("#venue")}>
+                   Venue
+                 </a>
+                 <a href="#addons" onClick={() => navigateToProducts("#addons")}>
+                   Add-ons
+                 </a>
+                 <a href="#meals" onClick={() => navigateToProducts("#meals")}>
+                   Meals
+                 </a>
+               </div>
+               <button
+                 className="details_button"
+                 onClick={() => setShowItems(!showItems)}
+               >
+                 Show Details
+               </button>
+        </div>
+      </navbar>
 
       <div className="main_container">
         {!showItems ? (
